e2951bfc72677dc0a7c4aa1be45ebbf3937c29dc
GabrielRobertoni <gabrielrober2017@gmail.com>
2026-05-08 00:31:13 -0300

    Add files via upload

---
+++ cp2_brinquedos_v2/src/main/java/br/com/fiap/cp2/Cp2BrinquedosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2BrinquedosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp2BrinquedosApplication.class, args);
+	}
+
+}
