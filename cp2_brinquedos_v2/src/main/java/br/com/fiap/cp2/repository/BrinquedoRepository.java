9ac080b160df6e5be93e5f1fbf6999d609fc4eab
GabrielRobertoni <gabrielrober2017@gmail.com>
2026-05-08 00:28:22 -0300

    Delete cp2_brinquedos_v2 directory

--- cp2_brinquedos_v2/src/main/java/br/com/fiap/cp2/repository/BrinquedoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.cp2.repository;
-
-import br.com.fiap.cp2.entity.Brinquedo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrinquedoRepository extends JpaRepository<Brinquedo, Long> {
-}
